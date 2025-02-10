eac53e6bfc31366ea02a51a0e215663bf90eceeb
Muhammad Asif <programmerasif@outlook.com>
2025-02-10 09:02:00 +0500

    list and tuple added

---
+++ chapter-3/problem2.py
@@ -0,0 +1,12 @@
+#strings are immutable
+
+#Find method returns the index of the found string
+
+name =  "asif is a good boy"
+print(name.find("good"))
+
+
+
+#Replace method
+name =  "asif is a good boy"
+print(name.replace("good","great"))
---
+++ chapter-4/list.py
@@ -0,0 +1,8 @@
+#lists are mutable
+
+fruits = ["apple", "orage","mango","lemon","stobray"]
+fruit1 = fruits[0]
+fruits.reverse()
+print(fruits[1:3])
+print(fruits)
+
